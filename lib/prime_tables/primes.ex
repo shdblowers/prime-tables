4626e8893efc15b807ab99cf288878cfaf56c853
Steven Blowers <shdblowers@gmail.com>
2016-05-15 12:57:06 +0100

    Refactoring is_prime? function.

--- lib/prime_tables/primes.ex
+++ lib/prime_tables/primes.ex
@@ -16,7 +16,6 @@ defmodule PrimeTables.Primes do
     case number do
       1 -> false
       2 -> true
-      3 -> true
       _ -> not Enum.any?((number - 1)..2, fn(x) ->
         rem(number, x) == 0
       end)
