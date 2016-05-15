5913ecbea107eb53a2d8225e6b21b941151f01e9
Steven Blowers <shdblowers@gmail.com>
2016-05-15 13:44:51 +0100

    Function to create a multiplication table from two lists.

--- lib/prime_tables/tables.ex
+++ lib/prime_tables/tables.ex
@@ -1,7 +1,8 @@
 defmodule PrimeTables.Tables do
 
   def create_multiplication_table(columns, rows) do
-    [[],[]]
+      [[nil] ++ columns] ++
+      Enum.map(rows, &create_multiplication_row(&1, [1] ++ columns))
   end
 
   def create_multiplication_row(multiplier, list) do
