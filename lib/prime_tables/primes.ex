1663fc78278e9ebc0ac7566b56ecc277631a206c
Steven Blowers <shdblowers@gmail.com>
2016-05-15 12:14:14 +0100

    Creating function to determine if a number is a prime.

--- lib/prime_tables/primes.ex
+++ lib/prime_tables/primes.ex
@@ -1,7 +1,14 @@
 defmodule PrimeTables.Primes do
 
   def is_prime?(number) do
-    true
+    case number do
+      1 -> false
+      2 -> true
+      3 -> true
+      _ -> not Enum.any?((number - 1)..2, fn(x) ->
+        rem(number, x) == 0
+      end)
+    end
   end
 
 end
