34d7896d155b20d6877db9f357f998ae42ab6844
Steven Blowers <shdblowers@gmail.com>
2016-05-15 22:33:36 +0100

    Adding missing test coverage for overall app behaviour.

--- test/prime_tables_test.exs
+++ test/prime_tables_test.exs
@@ -1,8 +1,20 @@
 defmodule PrimeTablesTest do
-  use ExUnit.Case
-  doctest PrimeTables
+  use ExUnit.Case, async: true
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import PrimeTables
+  import ExUnit.CaptureIO
+
+  test "get prime multiplication table from user input" do
+    result = capture_io fn ->
+      PrimeTables.main(["3"])
+    end
+
+    assert result ==
+    """
+    |   |  2 |  3 |  5 |
+    | 2 |  4 |  6 | 10 |
+    | 3 |  6 |  9 | 15 |
+    | 5 | 10 | 15 | 25 |
+    """
   end
 end
